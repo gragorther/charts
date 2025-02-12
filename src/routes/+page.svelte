a3e59cd2bd14c321b6ae6456dc51dddb9e54db7d
Gregor Podjed <gregor@gregtech.eu>
2025-02-12 19:41:54 +0100

    Upload

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,7 +12,8 @@
 		LinearScale,
 		PointElement,
 		ArcElement,
-		CategoryScale
+		CategoryScale,
+		elements
 	} from 'chart.js';
 
 	ChartJS.register(
@@ -28,29 +29,64 @@
 	type ChartData = {
 		labels: (string | number)[];
 		datasets: {
-			label: string;
-			fill: boolean;
-			borderColor: string;
-			data: number[];
+			label?: string;
+			fill?: boolean;
+			borderColor?: string;
+			data?: number[];
 			backgroundColor?: string[];
 			hoverBackgroundColor?: string[];
 		}[];
 	};
-
-	let chart1: ChartData = {
-		labels: ['A', 'B', 'C', 'D'],
+	function getAverage(array: number[]) {
+		let sum = 0;
+		for (let i = 0; i < array.length; i++) {
+			sum += array[i];
+		}
+		return sum / array.length;
+	}
+	let output = 0;
+	let output2 = 0;
+	data.forEach((element) => (output += element.f));
+	data.forEach((element) => (output2 += element.m));
+	output = output / data.length;
+	const output3 = (output + output2) / 2;
+	const chart1: ChartData = {
+		labels: data.map((element) => element.year),
 		datasets: [
 			{
-				label: 'Linija 1',
+				label: 'Male',
 				fill: false,
 				borderColor: 'rgb(54, 162, 235)',
-				data: [14, 1, 3, 15]
+				data: data.map((element) => element.m)
 			},
 			{
-				label: 'Linija 2',
+				label: 'Female',
 				fill: false,
 				borderColor: 'rgb(255, 99, 132)',
-				data: [12, 9, 30, 1]
+				data: data.map((element) => element.f)
+			},
+			{
+				label: 'Average',
+				fill: false,
+				borderColor: 'rgb(100, 99, 132)',
+				data: data.map((element) => (element.m + element.f) / 2)
+			},
+			{
+				label: 'Line',
+				fill: false,
+				borderColor: 'rgb(70, 255, 255)',
+				data: Array(data.length).fill(output3)
+			}
+		]
+	};
+
+	let chart2: ChartData = {
+		labels: ['Male', 'Female'],
+		datasets: [
+			{
+				data: [12, 12],
+
+				backgroundColor: ['rgb(12, 123, 123', 'rgb(123,12,123)']
 			}
 		]
 	};
@@ -61,6 +97,10 @@
 		<div class="border-4 border-violet-200 p-3">
 			<h2 class="text-center text-2xl text-violet-700 font-bold">Graf podatkov</h2>
 			<Line data={chart1} options={{ responsive: true }} />
+		</div>
+		<div class="border-4 border-violet-200 p-3">
+			<h2 class="text-center text-2xl text-violet-700 font-bold">Graf podatkov</h2>
+			<Pie data={chart2} options={{ responsive: true }} />
 		</div>
 	</div>
 </div>
