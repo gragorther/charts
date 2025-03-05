265750b302e0651cfbd1e7414cb9b2e50c584157
Gregor <gregor@gregtech.eu>
2025-03-05 19:45:23 +0100

    spremembe, waow

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -49,7 +49,7 @@
 	data.forEach((element) => (output += element.f));
 	data.forEach((element) => (output2 += element.m));
 	output = output / data.length;
-	const output3 = (output + output2) / 2;
+	//const output3 = (output + output2) / 2;
 	const chart1: ChartData = {
 		labels: data.map((element) => element.year),
 		datasets: [
@@ -64,32 +64,68 @@
 				fill: false,
 				borderColor: 'rgb(255, 99, 132)',
 				data: data.map((element) => element.f)
-			},
-			{
-				label: 'Average',
-				fill: false,
-				borderColor: 'rgb(100, 99, 132)',
-				data: data.map((element) => (element.m + element.f) / 2)
-			},
-			{
-				label: 'Line',
-				fill: false,
-				borderColor: 'rgb(70, 255, 255)',
-				data: Array(data.length).fill(output3)
 			}
+			// {
+			// 	label: 'Average',
+			// 	fill: false,
+			// 	borderColor: 'rgb(100, 99, 132)',
+			// 	data: data.map((element) => (element.m + element.f) / 2)
+			// },
+			// {
+			// 	label: 'Line',
+			// 	fill: false,
+			// 	borderColor: 'rgb(70, 255, 255)',
+			// 	data: Array(data.length).fill(output3)
+			// }
 		]
 	};
+	function getDataByYear(year: number) {
+		const output = data.find((element) => element.year == year);
+		// return [output?.m, output?.f];
+		if (output) return [output.m, output.f];
+		else return null;
+	}
+	let year: number = 2011;
 
 	let chart2: ChartData = {
 		labels: ['Male', 'Female'],
 		datasets: [
 			{
-				data: [12, 12],
+				data: getDataByYear(year) ?? [0, 0],
 
 				backgroundColor: ['rgb(12, 123, 123', 'rgb(123,12,123)']
 			}
 		]
 	};
+
+	function maxDiff() {
+		let max = [0, 0];
+		data.forEach((year) => {
+			let diff = Math.abs(year.f - year.m);
+
+			if (diff > max[1]) {
+				max = [year.year, diff];
+			}
+		});
+		return max;
+	}
+
+	function maxDiffPerc() {
+		let max = [0, 0];
+		data.forEach((year) => {
+			let total = year.m + year.f;
+			let diff = Math.abs((year.f / total - year.m / total) * 100);
+			if (diff > max[1]) {
+				max = [year.year, diff];
+			}
+		});
+		return max;
+	}
+
+	console.log(maxDiff());
+	$: {
+		chart2.datasets[0].data = getDataByYear(year) ?? [0, 0];
+	}
 </script>
 
 <div class="container mx-auto">
@@ -99,8 +135,23 @@
 			<Line data={chart1} options={{ responsive: true }} />
 		</div>
 		<div class="border-4 border-violet-200 p-3">
+			<input
+				class="border-red-500 border-2 justify-center"
+				type="number"
+				min="1954"
+				max="2023"
+				bind:value={year}
+			/>
 			<h2 class="text-center text-2xl text-violet-700 font-bold">Graf podatkov</h2>
 			<Pie data={chart2} options={{ responsive: true }} />
 		</div>
+	</div>
+	<div class="border-4 border-violet-200 p-3">
+		<h2 class="text-center text-2xl text-violet-700 font-bold">Največja razlika</h2>
+		<p>{maxDiff()[1]} v letu {maxDiff()[0]}.</p>
+	</div>
+	<div class="border-4 border-violet-200 p-3">
+		<h2 class="text-center text-2xl text-violet-700 font-bold">Največja razlika v procentih</h2>
+		<p>{maxDiffPerc()[1]}% v letu {maxDiffPerc()[0]}.</p>
 	</div>
 </div>
